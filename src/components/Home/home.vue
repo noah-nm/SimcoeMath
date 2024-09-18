a59d7d88547e3184cdcefe9d8f346e9f1b9ca950
noah-nm <noah.jr.mills@gmail.com>
2024-09-18 17:26:50 -0400

    Remove creditsfrom home page

--- src/components/Home/home.vue
+++ src/components/Home/home.vue
@@ -1,5 +1,4 @@
 <template>
     <h1 class="display-2 text-center mt-2">Welcome to Simcoe Math!</h1>
     <h2 class="text-center color-blue font-italic">A simple website to aid in the learning of foundational topics in mathematics.</h2>
-    <h2 class="text-center color-blue mt-10">Made By: Noah Mills, Ian Tapply, and Nathan Steele.</h2>
 </template>
