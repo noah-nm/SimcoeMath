47a3c7a9eaad0201cc676bb1bed804df286d4c48
noah-nm <87618379+noah-nm@users.noreply.github.com>
2024-11-03 15:10:14 -0500

    Add settings to octal to binary (#38)

--- src/components/DataStorage/OctalToBinaryPractice.vue
+++ src/components/DataStorage/OctalToBinaryPractice.vue
@@ -1,69 +1,108 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" md="5">
-        <h1 class="text-center">Convert the Octal Number to Binary</h1>
+  <v-container fluid class="all">
+    <v-row align="flex-start">
+      <v-col class="settings-panel" cols="12" md="3">
+        <v-expansion-panels>
+          <v-expansion-panel
+            title="Settings"
+            text="Modify game settings"
+            expand-icon="mdi-cog"
+            collapse-icon="mdi-cog"
+          >
+            <v-expansion-panel-text>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    width="160"
+                    placeholder="octal size"
+                    hide-details
+                    v-model="octalSize"
+                  ></v-text-field>
+                </v-col>
+                <v-col>
+                  <v-btn
+                    color="primary"
+                    class="mt-3 ml-10"
+                    @click="applySettings"
+                  >
+                    Apply</v-btn
+                  >
+                </v-col>
+              </v-row>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
 
-        <v-row class="mt-8">
-          <v-col>
-            <h3 class="text-center">
-              Octal Number:
-              <strong class="text-blue">{{ octalNumber }}</strong>
-            </h3>
-          </v-col>
-        </v-row>
+      <v-col class="game" cols="12" md="6">
+        <div class="centered-content">
+          <h1 class="text-center mr-6">Convert the octal number to binary</h1>
 
-        <v-row class="mb-4 mt-7">
-          <v-col>
-            <v-text-field
-              v-if="!reveal"
-              v-model="userInput"
-              label="Enter Binary Value"
-              type="input"
-            ></v-text-field>
-            <v-text-field
-              v-else-if="reveal"
-              v-model="userInput"
-              disabled
-              label="Enter Binary Value"
-              type="input"
-            ></v-text-field>
-          </v-col>
-        </v-row>
+          <v-row class="mt-8">
+            <v-col>
+              <h3 class="text-center">
+                Octal Number:
+                <strong class="text-blue">{{ octalNumber }}</strong>
+              </h3>
+            </v-col>
+          </v-row>
 
-        <v-row justify="center">
-          <v-col class="d-flex" cols="auto">
-            <v-btn class="mr-6 pr-6 pl-6" @click="generateOctal" color="primary"
-              >New Number</v-btn
-            >
-            <v-btn class="mr-6" @click="checkAnswer" color="success"
-              >Check Answer</v-btn
-            >
-            <v-btn color="primary" @click="revealAnswer">Reveal Answer</v-btn>
-          </v-col>
-        </v-row>
+          <v-row class="mb-4 mt-7">
+            <v-col>
+              <v-text-field
+                v-if="!reveal"
+                v-model="userInput"
+                label="Enter Binary Value"
+                type="input"
+              ></v-text-field>
+              <v-text-field
+                v-else-if="reveal"
+                v-model="userInput"
+                disabled
+                label="Enter Binary Value"
+                type="input"
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-        <v-row>
-          <v-col>
-            <v-alert
-              v-if="result !== null"
-              :type="result.correct ? 'success' : 'error'"
-              closable
-              @click:close="handleAlertDismiss"
-            >
-              {{ result.message }}
-            </v-alert>
-          </v-col>
-        </v-row>
+          <v-row justify="center">
+            <v-col class="d-flex ml-7" cols="auto">
+              <v-btn
+                class="mr-6 pr-6 pl-6"
+                @click="generateOctal"
+                color="primary"
+              >
+                New Number
+              </v-btn>
+              <v-btn class="mr-6" @click="checkAnswer" color="success">
+                Check Answer
+              </v-btn>
+              <v-btn color="primary" @click="revealAnswer">Reveal Answer</v-btn>
+            </v-col>
+          </v-row>
 
-        <v-row>
-          <v-col>
-            <h2 class="text-center">
-              The Correct Answer is:
-              <strong v-if="reveal">{{ octalBinary }}</strong>
-            </h2>
-          </v-col>
-        </v-row>
+          <v-row>
+            <v-col>
+              <v-alert
+                v-if="result !== null"
+                :type="result.correct ? 'success' : 'error'"
+                closable
+                @click:close="handleAlertDismiss"
+              >
+                {{ result.message }}
+              </v-alert>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col class="ml-6">
+              <h2 class="text-center">
+                The Correct Answer is:
+                <strong v-if="reveal">{{ octalBinary }}</strong>
+              </h2>
+            </v-col>
+          </v-row>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -80,9 +119,12 @@ export default defineComponent({
     const result = ref<{ correct: boolean; message: string } | null>(null);
     const reveal = ref<boolean>(false);
     const octalBinary = ref<string>("");
+    const octalSize = ref<number>(4);
 
     function generateOctal() {
-      const randomOctal = Math.floor(Math.random() * 512).toString(8);
+      const randomOctal = Array.from({ length: octalSize.value }, () =>
+        Math.floor(Math.random() * 8),
+      ).join("");
       octalNumber.value = randomOctal;
       userInput.value = "";
       octalBinary.value = parseInt(randomOctal, 8).toString(2);
@@ -110,6 +152,12 @@ export default defineComponent({
       result.value = null;
     }
 
+    function applySettings() {
+      if (octalSize.value > 0) {
+        generateOctal();
+      }
+    }
+
     onMounted(() => {
       generateOctal();
       reveal.value = false;
@@ -125,6 +173,8 @@ export default defineComponent({
       checkAnswer,
       handleAlertDismiss,
       revealAnswer,
+      octalSize,
+      applySettings,
     };
   },
 });
