171c70798d307b409d134939dc274ff51831b602
noah-nm <noah.jr.mills@gmail.com>
2024-11-20 10:27:35 -0500

    Menu updates

--- src/components/Trigonometry/TrigonometryMenu.vue
+++ src/components/Trigonometry/TrigonometryMenu.vue
@@ -1,8 +1,8 @@
 <template>
-  <h1 class="display-2 text-center mt-2">Grade 10 Practice</h1>
+  <h1 class="display-2 text-center mt-2">Trigonometry Practice</h1>
   <h2 class="text-center font-italic">
-    Selection of various games related to topics covered in the Grade 10
-    academic math curriculum
+    Selection of various games related to trigonometry concepts covered in the
+    Canadian high-school curriculum
   </h2>
   <v-divider
     :thickness="2"
